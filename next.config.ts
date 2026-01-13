c8a29e5266f27c74714ad13f43c1bbb6c5d96b34
Adrian Ibarra Pérez <adrian.ibarra@afarvi.com>
2026-01-13 17:25:46 +0100

    Fix: Configure Next.js to trace and include prisma/dev.db

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
+    // @ts-ignore
+    outputFileTracingIncludes: {
+      '/api/**/*': ['./prisma/dev.db'],
+      '/login': ['./prisma/dev.db'],
+    },
+  },
 };
 
 export default nextConfig;
